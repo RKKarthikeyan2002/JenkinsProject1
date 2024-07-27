1fe4d9048356097c501a22916aeee6639a004fa5
Karthikeyan R K <rkkarthikeyan@gmail.com>
2024-07-27 16:53:12 +0530

    Test Added

---
+++ src/test/java/com/karthi/backendvendor/serviceimpli/VendorServiceImpliTest.java
@@ -0,0 +1,14 @@
+package com.karthi.backendvendor.serviceimpli;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class VendorServiceImpliTest {
+
+	@Test
+	void testAddVendor() {
+		assertEquals("true", "true");
+	}
+
+}
